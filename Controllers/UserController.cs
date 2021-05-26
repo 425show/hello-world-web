b05e27214fd59457ad7997c1e4932b84ee6b6a34
Christos Matskas <chmatsk@microsoft.com>
2021-05-26 11:49:33 -0700

    working code for demo

---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+namespace IdentityWeb.Controllers
+{
+  [Authorize]
+  public class UserController : Controller
+  {
+    private readonly ILogger<UserController> _logger;
+    private readonly GraphServiceClient _graphServiceClient;
+
+    public UserController(ILogger<UserController> logger, GraphServiceClient graphServiceClient)
+    {
+      _logger = logger;
+      _graphServiceClient = graphServiceClient;
+    }
+
+
+    [AuthorizeForScopes(Scopes = new[] { "User.Read" })]
+    public async Task<IActionResult> Index()
+    {
+      var users = await _graphServiceClient.Users
+        .Request()
+        .Top(10)
+        .GetAsync();
+
+      return View(users);
+    }
+  }
+}
